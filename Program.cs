b5944b4da544c770c5ba15c6d770e4c7b9ad26af
Patricia Souza <patricia.sz.br@gmail.com>
2025-09-20 13:27:00 -0300

    Declarando tipos de variaveis

--- Program.cs
+++ Program.cs
@@ -1,4 +1,11 @@
-﻿
-using ExemploFundamentos.Models;
+﻿string apresentacao = "Ola, seja bem-vindo";
+int quantidade = 1;
+double altura = 1.80;
+decimal preco = 1.80M;
+bool condicao = true;
 
-Pessoa p = new Pessoa();
+Console.WriteLine(apresentacao);
+Console.WriteLine("Valor da variavel quantidade " + quantidade);
+Console.WriteLine("Valor da variavel altura " + altura.ToString("0.00"));
+Console.WriteLine("Valor da variavel preco " + preco);
+Console.WriteLine("Valor da variavel condicao " + condicao);
