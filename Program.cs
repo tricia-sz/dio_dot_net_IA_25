3f08fe5bb69da9f02af55c222235fb688fcbd0b9
Patricia Souza <patricia.sz.br@gmail.com>
2025-09-21 09:42:04 -0300

    if else

--- Program.cs
+++ Program.cs
@@ -1,38 +1,20 @@
 ﻿using ExemploFundamentos.Models;
 
-//Cast - Casting
-// int a = Convert.ToInt32("5");
-
-// int a = int.Parse("5");
-
-// Console.WriteLine(a);
+int quantidadeEmEstoque = 10;
+int quantidadeCompra = 4;
+bool possivelVenda = quantidadeEmEstoque >= quantidadeCompra;
 
 
-int inteiro = 5;
+Console.WriteLine($"Quantidade em Estoque: {quantidadeEmEstoque}");
+Console.WriteLine($"Quantidade Compra: {quantidadeCompra}");
+Console.WriteLine($"É possivel realizar a venda? {possivelVenda}");
 
-string a = inteiro.ToString();
-
-Console.WriteLine(a);
-
-// int a = 10;
-// int b = 20;
-
-// int c = a + b;
-
-// c += 5;
-
-// Console.WriteLine(c);
-// c -= 2;
-// Console.WriteLine(c);
-
-// string apresentacao = "Ola, seja bem-vindo";
-// int quantidade = 1;
-// double altura = 1.80;
-// decimal preco = 1.80M;
-// bool condicao = true;
-
-// Console.WriteLine(apresentacao);
-// Console.WriteLine("Valor da variavel quantidade " + quantidade);
-// Console.WriteLine("Valor da variavel altura " + altura.ToString("0.00"));
-// Console.WriteLine("Valor da variavel preco " + preco);
-// Console.WriteLine("Valor da variavel condicao " + condicao);
+if (possivelVenda)
+{
+  Console.WriteLine("Venda realizada com sucesso.");
+}
+else
+{
+  Console.WriteLine("Desculpe. Nao temos a quantidade desejada em estoque.");
+  
+}
