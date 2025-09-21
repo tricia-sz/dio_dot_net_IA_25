9f1a2495f6713b96b8c194363787483b9f1cb8ff
Patricia Souza <patricia.sz.br@gmail.com>
2025-09-21 10:15:57 -0300

    if aninhado

--- Program.cs
+++ Program.cs
@@ -2,14 +2,17 @@
 
 int quantidadeEmEstoque = 10;
 int quantidadeCompra = 4;
-bool possivelVenda = quantidadeEmEstoque >= quantidadeCompra;
-
+bool possivelVenda = quantidadeCompra > 0 && quantidadeEmEstoque >= quantidadeCompra;
 
 Console.WriteLine($"Quantidade em Estoque: {quantidadeEmEstoque}");
 Console.WriteLine($"Quantidade Compra: {quantidadeCompra}");
 Console.WriteLine($"É possivel realizar a venda? {possivelVenda}");
 
-if (possivelVenda)
+if (quantidadeCompra == 0)
+{
+  Console.WriteLine("Venda inválida. Quantidade: 0");
+}
+else if (possivelVenda)
 {
   Console.WriteLine("Venda realizada com sucesso.");
 }
