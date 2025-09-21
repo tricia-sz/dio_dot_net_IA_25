cbc0fb40d9ed3f1e2f0ff8082d8214a73371b2a7
Patricia Souza <patricia.sz.br@gmail.com>
2025-09-21 12:21:25 -0300

    Funcoes tringonometricas

--- Models/Calculadora.cs
+++ Models/Calculadora.cs
@@ -29,6 +29,27 @@ namespace ExemploFundamentos.Models
       Console.WriteLine($"{x} ^ {y} = {pot}");
     }
 
+    public void Seno(double angulo)
+    {
+      double radiano = angulo * Math.PI / 180;
+      double seno = Math.Sin(radiano);
+      Console.WriteLine($"Seno de {angulo}° = {Math.Round(seno, 4)}");
+    }
+
+    public void Coseno(double angulo)
+    {
+      double radiano = angulo * Math.PI / 180;
+      double coseno = Math.Cos(radiano);
+      Console.WriteLine($"Coseno de {angulo}° = {Math.Round(coseno,4)})");
+    }
+
+    public void Tangente(double angulo)
+    {
+      double radiano = angulo * Math.PI / 180;
+      double tangete = Math.Tan(radiano);
+      Console.WriteLine($"Tangente de {angulo}° = {Math.Round(tangete, 4)}");
+    }
+
 
   }
 }
--- Program.cs
+++ Program.cs
@@ -7,4 +7,7 @@ Calculadora calc = new Calculadora();
 // calc.Multiplicar(5, 5);
 // calc.Dividir(20, 4);
 
-calc.Potencia(3, 3);
+// calc.Potencia(3,3);
+calc.Seno(9);
+calc.Coseno(6);
+calc.Tangente(3);
